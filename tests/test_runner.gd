14196df85b8e512d2551ad7ff6e88b7a858a3d1c
teodorstamenov <teodor.m.stamenov@gmail.com>
2026-07-25 20:42:35 +0300

    feat: Създаване на тестове за валиден и невалиден MatchConfig
    closes #28

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -30,6 +30,7 @@ const TEST_FILES: Array[String] = [
 	"res://tests/unit/domain/match_config_test.gd",
 	"res://tests/unit/domain/match_config_serialization_test.gd",
 	"res://tests/unit/domain/match_config_validator_test.gd",
+	"res://tests/unit/domain/match_config_validity_test.gd",
 	"res://tests/unit/domain/ai_difficulty_test.gd",
 	"res://tests/unit/domain/animal_id_test.gd",
 	"res://tests/unit/domain/theme_id_test.gd",
