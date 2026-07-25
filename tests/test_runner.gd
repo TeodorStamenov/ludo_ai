0995c74caf0a5c336710d18d3fc116b91086a258
teodorstamenov <teodor.m.stamenov@gmail.com>
2026-07-25 22:40:35 +0300

    feat: Създаване на TurnState модел
    closes #53

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -18,6 +18,7 @@ const TEST_FILES: Array[String] = [
 	"res://tests/unit/domain/pawn_zone_test.gd",
 	"res://tests/unit/domain/match_phase_test.gd",
 	"res://tests/unit/domain/turn_phase_test.gd",
+	"res://tests/unit/domain/turn_state_test.gd",
 	"res://tests/unit/domain/framework_sanity_test.gd",
 	"res://tests/unit/domain/random_source_test.gd",
 	"res://tests/unit/domain/seeded_random_source_test.gd",
